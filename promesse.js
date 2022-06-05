1e482c7431c1f0de9b03b79d460547d9e38ad345
raihan1610X <43400292+raihan1610X@users.noreply.github.com>
2022-06-05 15:30:53 +0700

    Delete promesse.js

--- promesse.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch')
-
-async function dans3secondes() {
-    const pokemon1 = await fetch('http://pokeapi.co/api/v2/pokemon/1')
-
-    return pokemon1.json()
-}
-
-dans3secondes().then( console.log )
